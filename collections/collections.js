7bb6a1187c74365f93c6ea4c6dba78605a6c4b29
tchamberlain <taylorannchamberlain@gmail.com>
2015-09-25 12:45:26 -0500

    going back to old, no maps, no mobile ui for features version

--- collections/collections.js
+++
@@ -1,6 +0,0 @@
-//initializing collections for whole project
-
-
-Pre_activities = new Mongo.Collection('pre_activities');
-Activities = new Mongo.Collection('activities');
-Invitations = new Mongo.Collection('invitations');
